02ddc54505514908aef3f5ecfab7da136c2e0e90
boukepieter <boukepieter.ottow@wur.nl>
2014-08-18 12:50:50 +0200

    added licence headers

--- license_header.h
+++ license_header.h
@@ -1,75 +1,28 @@
 /****************************************************************************
-** Copyright (C) 2014-2014 52ï¿½North Initiative for Geospatial Open Source
+** Copyright (C) 2014-%YEAR% 52°North Initiative for Geospatial Open Source
 ** Software GmbH
-**
+** 
 ** This program is free software; you can redistribute it and/or modify it
 ** under the terms of the GNU General Public License version 2 as published
 ** by the Free Software Foundation.
-**
+** 
 ** If the program is linked with libraries which are licensed under one of
 ** the following licenses, the combination of the program with the linked
 ** library is not considered a "derivative work" of the program:
-**
+** 
 **  - Apache License, version 2.0
 **  - Apache Software License, version 1.0
 **  - GNU Lesser General Public License, version 3
 **  - Mozilla Public License, versions 1.0, 1.1 and 2.0
 **  - Common Development and Distribution License (CDDL), version 1.0.
-**
+** 
 ** Therefore the distribution of the program linked with libraries licensed
 ** under the aforementioned licenses, is permitted by the copyright holders
 ** if the distribution is compliant with both the GNU General Public
 ** icense version 2 and the aforementioned licenses.
-**
+** 
 ** This program is distributed in the hope that it will be useful, but
 ** WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 ** Public License for more details.
 ****************************************************************************/
-
-// Author: B.P. Ottow
-// Date: August 2014
-// GSoC Project: Gatherer, ILWIS Mobile. Hosted by 52 North and ITC Enschede.
-//
-// This is the start of an observation, it has one or two tabs. Record observation and map.
-
-import QtQuick 2.2
-import QtQuick.Controls 1.2
-import QtQuick.Layouts 1.1
-
-Rectangle {id:page1Container
-    width: 768
-    height: 1030
-    color: "#d4d4d4"
-    signal handlerLoader(string name)
-
-    Connections {
-        target: controlPage.item
-        onHandlerLoader:{handlerLoader(name);
-        }
-
-    }
-
-    TabView {
-        id:frame
-        tabPosition: controlPage.item ? controlPage.item.tabPosition : Qt.TopEdge
-        anchors.fill: parent
-        anchors.margins: 2
-
-        Tab {
-            id: controlPage
-            title: "Observation"
-            active: true
-            Observation { }
-
-            Component.onCompleted: {
-                if (currentobservation.mapAvailable() === "true"){
-                    frame.addTab("Map", Qt.createComponent("Map.qml"))
-                    frame.moveTab(0,1)
-                }
-            }
-        }
-    }
-
-}
-
